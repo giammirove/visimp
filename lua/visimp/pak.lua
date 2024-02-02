1582882f453d40b106bfb9848347932490b4d2e4
Gianmaria Rovelli <rovelli.gianmaria@gmail.com>
2024-02-02 16:04:07 +0100

    accrocchio: added command to install a new plugin (exploiting register function)

--- lua/visimp/pak.lua
+++ lua/visimp/pak.lua
@@ -25,6 +25,7 @@ end
 
 do
   vim.tbl_map(vim.cmd, {
+    'command! -nargs=1 PakAdd lua local t=require(\'visimp.pak\') t.register({url = <q-args>}) vim.cmd(\'PakInstall\')',
     'command! PakInstall  lua require(\'visimp.pak\').run(\'install\')',
     'command! PakUpdate   lua require(\'visimp.pak\').run(\'update\')',
     'command! PakClean    lua require(\'visimp.pak\').run(\'clean\')',
